60747dec4c9ccc3a8834ecec6d6fd60c006052db
Jordyn Muldrow <48972362+jramuldrow@users.noreply.github.com>
2019-04-13 16:32:05 -0700

    Update aff_a.c

--- aff_a.c
+++ aff_a.c
@@ -20,18 +20,18 @@ int   main(int argc, char **argv)
   int i;
   
   i = 0;
-  if (argc != 2)
+  if (argc != 2) // if the argument is not 2 then will display "a" in the output.
     write(1, "a", 1);
   else
   {
-    while(argv[1][i])
+    while(argv[1][i]) // while were at the index of the first argument
     { 
-        if(argv[1][i] == 'a')
+        if(argv[1][i] == 'a') // if the index id equal to "a" 
         {
-          write(1, "a", 1);
-          break ;
+          write(1, "a", 1); // then write "a" to the standard output
+          break ; // will break out of the loop once we encounter the first "a"
         }
-        i += 1;
+        i += 1; // this will work until we enouter "a" or until we have gone though the whole string.
     }
   }
   write(1, "\n", 1);
